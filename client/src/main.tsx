eeea11aff39e28050363eb9b1de2d1edf4bb64b4
Christiaan Seerden <christiaanseerden@live.nl>
2025-01-13 00:28:05 +0100

    set up new packages on the client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,3 @@
-import App from "@/App";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
@@ -11,9 +10,20 @@ if (process.env.NODE_ENV === "development") {
 	});
 }
 
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
