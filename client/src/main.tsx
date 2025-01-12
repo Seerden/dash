fa9a8e423201e33d8cc3aa57441f6d3bba362aa1
Christiaan Seerden <christiaanseerden@live.nl>
2025-01-13 00:41:13 +0100

    use theme and query providers, set up query client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@emotion/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
@@ -10,6 +11,9 @@ if (process.env.NODE_ENV === "development") {
 	});
 }
 
+import { queryClient } from "@/lib/query-client";
+import { theme } from "@/lib/theme";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
@@ -24,6 +28,10 @@ declare module "@tanstack/react-router" {
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<QueryClientProvider client={queryClient}>
+			<ThemeProvider theme={theme}>
+				<RouterProvider router={router} />
+			</ThemeProvider>
+		</QueryClientProvider>
 	</React.StrictMode>
 );
