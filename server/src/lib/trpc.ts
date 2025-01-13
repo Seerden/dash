22d1fed40ecfeaf69c0eac8381b0ea4f065489f5
Christiaan Seerden <christiaanseerden@live.nl>
2025-01-13 22:17:12 +0100

    mess around with trpc procedures

--- server/src/lib/trpc.ts
+++ server/src/lib/trpc.ts
@@ -1,11 +1,13 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import type * as trpcExpress from "@trpc/server/adapters/express";
 import SuperJSON from "superjson";
 import { z } from "zod";
 
 export const createContext = ({ req, res }: trpcExpress.CreateExpressContextOptions) => ({
 	hello: "world",
-	session: req.session.user, // TODO: this should only be applied to the authenticated procedure.
+	session: {
+		user: req.session.user,
+	}, // TODO: this should only be applied to the authenticated procedure.
 });
 type Context = Awaited<ReturnType<typeof createContext>>;
 
@@ -13,9 +15,40 @@ const t = initTRPC.context<Context>().create({
 	transformer: SuperJSON,
 });
 
+export const publicProcedure = t.procedure.use(async (opts) => {
+	try {
+		return opts.next(opts);
+	} catch (error) {
+		// do something here? or use router's onError?
+		return opts.next(opts);
+	}
+});
+
+export const authenticatedProcedure = publicProcedure.use(async (opts) => {
+	if (!opts.ctx.session.user) {
+		throw new TRPCError({
+			code: "UNAUTHORIZED",
+			message: "Must be logged in to access this resource.",
+		});
+	} else {
+		return opts.next(opts);
+	}
+});
+
 export const appRouter = t.router({
-	hello: t.procedure.input(z.object({ name: z.string() })).query(({ input, ctx }) => {
-		return { message: `Hello, ${input.name}!`, hello: ctx.hello };
+	hello: publicProcedure
+		.input(z.object({ name: z.string() }))
+		.query(({ input, ctx }) => {
+			return { message: `Hello, ${input.name}!` };
+		}),
+	bye: publicProcedure.input(z.object({ name: z.string() })).query(({ input, ctx }) => {
+		return { message: `Hello, ${input.name}!` };
+	}),
+	me: authenticatedProcedure.query(({ ctx }) => {
+		return {
+			user: ctx.session.user,
+			who: "am i",
+		};
 	}),
 });
 
