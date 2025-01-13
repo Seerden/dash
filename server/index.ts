8a81a0d8c7852e57ccbd000d8ab996e06508994a
Christiaan Seerden <christiaanseerden@live.nl>
2025-01-13 01:01:11 +0100

    set up server as a regular express app

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,45 @@
-async function start() {}
+import cors from "cors";
+import express from "express";
+import session from "express-session";
+import { pingDatabase } from "./src/db/init";
+import { initializeRedisConnection, redisSession } from "./src/lib/redis-client";
+import { runAtStartup } from "./src/lib/run-at-startup";
+
+async function start() {
+	const app = express();
+
+	app.use(
+		cors({
+			origin: true,
+			credentials: true,
+		}),
+	);
+
+	app.use(
+		express.urlencoded({
+			limit: "10mb",
+			parameterLimit: 10000,
+			extended: true,
+		}),
+	);
+
+	await initializeRedisConnection();
+	await pingDatabase();
+	app.use(session(redisSession));
+
+	app.use(express.json()); // TODO: is this still necessary with trpc? they use superjson as transformer
+
+	app.get("/", async (req, res) => {
+		res.json({ message: "Hello World" });
+	});
+
+	const port = process.env.PORT ?? 5000;
+
+	await runAtStartup();
+
+	app.listen(port, () => {
+		console.log(`${new Date().toISOString()}: server is running on port ${port}`);
+	});
+}
 
 start();
---
+++ server/src/lib/run-at-startup.ts
@@ -0,0 +1,3 @@
+export async function runAtStartup() {
+	// Do something here.
+}
