caf3a623dd2b959e7c3afbe7968a181490a234cf
Christiaan Seerden <christiaanseerden@live.nl>
2025-01-13 21:15:11 +0100

    tried to get client to run in docker -- did not work

--- server/.eslintrc.cjs
+++ server/.eslintrc.cjs
@@ -1,10 +1,14 @@
 module.exports = {
 	env: {
-		browser: true,
 		es2021: true,
 		node: true,
 	},
-	extends: ["eslint:recommended", "plugin:@typescript-eslint/strict", "prettier"],
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/strict",
+		"prettier",
+		"plugin:json/recommended-legacy",
+	],
 	parser: "@typescript-eslint/parser",
 	parserOptions: {
 		ecmaFeatures: {
@@ -23,11 +27,13 @@ module.exports = {
 	rules: {
 		"@typescript-eslint/consistent-type-imports": "error",
 	},
+	ignorePatterns: ["node_modules", "build", "dist"],
 	overrides: [
 		{
 			files: ["**/*.js", "**/*.ts"],
 			rules: {
 				"@typescript-eslint/no-unused-vars": "off",
+				"@typescript-eslint/no-unused-expressions": "error",
 				"@typescript-eslint/explicit-module-boundary-types": "off",
 				"no-unused-vars": "off",
 			},
