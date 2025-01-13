ec96c08e0666e6cf1dff0d3f62266127ae9aa331
Christiaan Seerden <48338210+Seerden@users.noreply.github.com>
2025-01-13 01:32:00 +0100

    DAS-4: server setup (#15)
    * set up server as a regular express app
    
    * install zod
    
    * server trpc setup, and import dotenv/config to sessions work
    
    * do not console.log environment variables

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,56 @@
-async function start() {}
+import * as trpcExpress from "@trpc/server/adapters/express";
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import session from "express-session";
+import { pingDatabase } from "./src/db/init";
+import { initializeRedisConnection, redisSession } from "./src/lib/redis-client";
+import { runAtStartup } from "./src/lib/run-at-startup";
+import { appRouter, createContext } from "./src/lib/trpc";
+
+async function start() {
+	const app = express();
+
+	app.use(
+		cors({
+			origin: true,
+			credentials: true,
+		}),
+	);
+
+	app.use(
+		express.urlencoded({
+			limit: "10mb",
+			parameterLimit: 10000,
+			extended: true,
+		}),
+	);
+
+	await initializeRedisConnection();
+	await pingDatabase();
+	app.use(session(redisSession));
+
+	app.use(express.json()); // TODO: is this still necessary with trpc? they use superjson as transformer
+
+	app.get("/", async (req, res) => {
+		res.json({ message: "Hello World" });
+	});
+
+	app.use(
+		"/trpc",
+		trpcExpress.createExpressMiddleware({
+			router: appRouter,
+			createContext,
+		}),
+	);
+
+	const port = process.env.PORT ?? 5000;
+
+	await runAtStartup();
+
+	app.listen(port, () => {
+		console.log(`${new Date().toISOString()}: server is running on port ${port}`);
+	});
+}
 
 start();
