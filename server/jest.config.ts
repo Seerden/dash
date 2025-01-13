759af6b887ce314e3c0984905ae1374a6ed28d7a
Christiaan Seerden <christiaanseerden@live.nl>
2025-01-14 00:59:36 +0100

    resolve path aliases in tests

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -1,5 +1,16 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+import { pathsToModuleNameMapper } from "ts-jest";
+// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
+// which contains the path mapping (ie the `compilerOptions.paths` option):
+import * as json from "./tsconfig.json";
+
 module.exports = {
 	preset: "ts-jest",
+	roots: ["<rootDir>"],
 	testEnvironment: "node",
+	modulePaths: [json.compilerOptions.baseUrl],
+	moduleNameMapper: pathsToModuleNameMapper(
+		json.compilerOptions.paths /*, { prefix: '<rootDir>/' } */,
+	),
 };
