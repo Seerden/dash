7f9c96c9b6c34dc28570b36415a52443d164532a
Christiaan Seerden <christiaanseerden@live.nl>
2025-01-13 01:45:26 +0100

    set up trpc on client

---
+++ client/src/lib/trpc.ts
@@ -0,0 +1,24 @@
+import { createTRPCClient, httpBatchLink } from "@trpc/client";
+import type { AppRouter } from "server/src/lib/trpc";
+//     👆 **type-only** import
+
+// Pass AppRouter as generic here. 👇 This lets the `trpc` object know
+// what procedures are available on the server and their input/output types.
+export const trpc = createTRPCClient<AppRouter>({
+	links: [
+		httpBatchLink({
+			// TODO: use a baseUrl variable for this
+			url: "http://localhost:5000/trpc",
+			fetch(url, options) {
+				return fetch(url, {
+					...options,
+					headers: {
+						...options?.headers,
+						"Content-Type": "application/json"
+					},
+					credentials: "include"
+				});
+			}
+		})
+	]
+});
