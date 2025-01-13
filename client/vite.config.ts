de2a3e596a83b980ba07cc95e8efd123a76730d8
Christiaan Seerden <christiaanseerden@live.nl>
2025-01-13 21:21:27 +0100

    yarn add @types/node

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,10 +7,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [TanStackRouterVite(), react(), tsconfigPaths()],
-   esbuild: {
-      target: "esnext",
-      platform: "linux"
-   },
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "../client/src"),
@@ -21,8 +17,7 @@ export default defineConfig({
 		extensions: [".js", ".jsx", ".ts", ".tsx", ".json"]
 	},
 	server: {
-      host: true,
-      port: 5173,
+		port: 5173,
 		watch: {
 			usePolling: true
 		}
