0cd135d2065cc0d07e98a3b360de8893a14b47e4
Christiaan Seerden <48338210+Seerden@users.noreply.github.com>
2025-01-13 13:37:21 +0100

    DAS-14:  set up `trpc` (#16)
    * include server trpc file in client tsconfig
    
    * set up trpc on client
    
    * install and use superjson as trpc validator, install zod in shared folder
    
    * extend context request type to recognize session user
    
    * install @trpc/react-query
    
    * use trpc/react-query abstraction and re-organize client setup to use App component
    
    * clean up various comments etc.

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,42 +1,6 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
-import React from "react";
-
-const TanStackRouterDevtools =
-	process.env.NODE_ENV === "production"
-		? () => null // Render nothing in production
-		: React.lazy(() =>
-				// Lazy load in development
-				import("@tanstack/router-devtools").then((res) => ({
-					default: res.TanStackRouterDevtools
-					// For Embedded Mode
-					// default: res.TanStackRouterDevtoolsPanel
-				}))
-			);
-
-const ReactQueryDevtools =
-	process.env.NODE_ENV === "production"
-		? () => null
-		: React.lazy(() =>
-				import("@tanstack/react-query-devtools").then((res) => ({
-					default: res.ReactQueryDevtools
-				}))
-			);
+import App from "@/App";
+import { createRootRoute } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
-	component: () => (
-		<>
-			<div className="p-2 flex gap-2">
-				<Link to="/" className="[&.active]:font-bold">
-					Home
-				</Link>{" "}
-				<Link to="/about" className="[&.active]:font-bold">
-					About
-				</Link>
-			</div>
-			<hr />
-			<Outlet />
-			<TanStackRouterDevtools />
-			<ReactQueryDevtools initialIsOpen={false} position="bottom" />
-		</>
-	)
+	component: App
 });
