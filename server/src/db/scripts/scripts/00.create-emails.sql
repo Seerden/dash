348b621c53a53cfdbb7bc85dadba75f1699a7c12
Christiaan Seerden <christiaanseerden@live.nl>
2025-01-18 21:35:15 +0100

    move db script into scripts subfolder

--- server/src/db/scripts/scripts/00.create-emails.sql
+++ server/src/db/scripts/scripts/00.create-emails.sql
@@ -1,15 +1,15 @@
 -- this table mirrors the shape of the email object returned by the Resend API
 CREATE TABLE if NOT EXISTS emails (
    id text NOT NULL PRIMARY KEY,
-   object text NOT NULL,
+   "object" text NOT NULL,
    reply_to text[],
-   from text NOT NULL,
+   "from" text NOT NULL,
    last_event text NOT NULL,
-   to text[] NOT NULL,
+   "to" text[] NOT NULL,
    cc text[],
    bcc text[],
-   subject text NOT NULL,
-   text text,
+   "subject" text NOT NULL,
+   "text" text,
    html text,
    scheduled_at text ,
    created_at text NOT NULL
