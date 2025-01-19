7ea7c7f0cfd30cc8c989c7cea0d35af01b2208bb
Christiaan Seerden <christiaanseerden@live.nl>
2025-01-19 14:12:13 +0100

    (types) implement OmitStrict, extend user types, add is_active, fix import paths

--- shared/src/types/utility.types.ts
+++ shared/src/types/utility.types.ts
@@ -5,3 +5,10 @@ export type Nullable<T> = T | null;
 export type ID = string;
 
 export type Maybe<T> = T | null | undefined;
+
+/**
+ * Omit that only works with keys actually present in the object.
+ * @see https://github.com/microsoft/TypeScript/issues/30825#issuecomment-673002409 */
+export type OmitStrict<T, K extends keyof T> = T extends any
+	? Pick<T, Exclude<keyof T, K>>
+	: never;
