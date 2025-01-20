8300a83a28cd292f0c1cd73573ae2d554bf3cb23
Christiaan Seerden <christiaanseerden@live.nl>
2025-01-20 17:58:26 +0100

    simplify httpBatchLink options

--- client/src/lib/use-trpc-client.ts
+++ client/src/lib/use-trpc-client.ts
@@ -8,21 +8,16 @@ const HOST = import.meta.env.NODE_ENV === "production" ? DOMAIN : "localhost";
 
 export default function useTRPCClient() {
 	const url = `http://${HOST}:5000/api/trpc`;
-	console.log({ url });
 
 	const [trpcClient] = useState(() =>
 		trpc.createClient({
 			links: [
 				httpBatchLink({
 					transformer: SuperJSON,
-					url: `http://${HOST}:5000/api/trpc`, // TODO: use a baseUrl variable for this
+					url,
 					fetch(url, options) {
 						return fetch(url, {
 							...options,
-							headers: {
-								...options?.headers,
-								"Content-Type": "application/json"
-							},
 							credentials: "include"
 						});
 					}
