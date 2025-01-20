e57d774b863d76236d52d16a77594992a29111e2
Christiaan Seerden <christiaanseerden@live.nl>
2025-01-20 17:59:39 +0100

    fix appRouter type in the client

--- client/src/lib/trpc.ts
+++ client/src/lib/trpc.ts
@@ -1,4 +1,11 @@
 import type { AppRouter } from "@server/lib/trpc";
 import { createTRPCReact } from "@trpc/react-query";
 
-export const trpc = createTRPCReact<AppRouter>();
+/**
+ * Have a bug with type inference, even though our tsconfig is otherwise set up
+ * perfectly fine. Using the type annotation as below, makes the error go away.
+ * @see https://github.com/pnpm/pnpm/issues/6089
+ * @see https://github.com/microsoft/TypeScript/issues/42873#issuecomment-1416128545
+ */
+export const trpc: ReturnType<typeof createTRPCReact<AppRouter>> =
+	createTRPCReact<AppRouter>();
