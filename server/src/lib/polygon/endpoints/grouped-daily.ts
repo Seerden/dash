f74bd8a816f6eaca7143def19ffe84261b44ec84
Christiaan Seerden <christiaanseerden@live.nl>
2025-01-21 19:27:31 +0100

    initial implementation of polygon fetch functions

---
+++ server/src/lib/polygon/endpoints/grouped-daily.ts
@@ -0,0 +1,20 @@
+import { fetchPolygon } from "@/lib/polygon/fetch";
+import type { OHLCVResult } from "@/lib/polygon/polygon.types";
+import type { YearMonthDay } from "types/data.types";
+
+export const getGroupedDailyUrl = (date: YearMonthDay) =>
+	`v2/aggs/grouped/locale/us/market/stocks/${date}`;
+
+/** Fetches from Polygon's "grouped daily (Bars)"" endpoint.
+ * @see https://polygon.io/docs/stocks/get_v2_aggs_grouped_locale_us_market_stocks__date
+ */
+export async function fetchGroupedDaily(date: YearMonthDay) {
+	const data = await fetchPolygon<OHLCVResult>({
+		endpoint: getGroupedDailyUrl(date),
+		params: {
+			include_otc: false,
+		},
+	});
+
+	return data;
+}
