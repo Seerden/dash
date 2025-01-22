a6ffd3cca428935d11ea5efe5b484034d3aa0d1b
Christiaan Seerden <christiaanseerden@live.nl>
2025-01-22 23:07:11 +0100

    (types) typedObjectEntries

--- server/types/utility.types.ts
+++ server/types/utility.types.ts
@@ -6,3 +6,9 @@ type WithSQL<T> = T & { sql?: SQL };
 
 /** Type helper for functions that interact with the database. */
 export type QueryFunction<TInput, TOutput> = (args: WithSQL<TInput>) => Promise<TOutput>;
+
+export type ObjectEntries<T> = [keyof T, T[keyof T]][];
+
+export function typedObjectEntries<T extends object>(obj: T) {
+	return Object.entries(obj) as ObjectEntries<T>;
+}
