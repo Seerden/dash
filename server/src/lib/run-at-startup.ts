37d62dc10970b354518543dfd795c42dc2bfa89a
Christiaan Seerden <christiaanseerden@live.nl>
2025-01-22 23:08:55 +0100

    mock insert many price action rows

--- server/src/lib/run-at-startup.ts
+++ server/src/lib/run-at-startup.ts
@@ -1,3 +1,22 @@
+import { sqlConnection } from "@/db/init";
+import { insertPriceAction } from "@/lib/data/models/price-action/insert-price-action";
+import type { PriceAction } from "types/price-action.types";
+
+function mockPriceAction(index: number): PriceAction {
+	return {
+		close: 2,
+		open: 1,
+		high: 3,
+		low: 0.5,
+		volume: 10,
+		ticker: "MSFT",
+		timestamp: index,
+	};
+}
+
+const mockManyPriceActionRows = () =>
+	Array.from({ length: 1e4 }, (_, i) => mockPriceAction(i));
+
 export async function runAtStartup() {
 	// const data = await fetchGroupedDaily("2023-09-01");
 	// console.log({ resultsCount: data?.resultsCount });
@@ -10,5 +29,20 @@ export async function runAtStartup() {
 	// 	to: "2025-01-20",
 	// });
 	// console.log({ aggregateData });
-	// await fs.writeFile("./aggregateData.json", JSON.stringify(aggregateData, null, 2));
+	// await fs.writeFile("./aggregateData.json", JSON.stringify(aggregateData,
+	// null, 2));
+
+	console.time("insertPriceAction");
+	const rows = mockManyPriceActionRows().slice(10);
+	console.log({ rows });
+	await sqlConnection.begin(async (sql) => {
+		const inserted = await insertPriceAction({
+			sql,
+			priceAction: rows,
+		});
+		console.log({ inserted });
+
+		await sql`rollback`;
+	});
+	console.timeEnd("insertPriceAction");
 }
