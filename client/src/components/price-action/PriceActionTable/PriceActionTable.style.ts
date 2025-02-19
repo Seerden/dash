9edbbf43ba0e77b074f136005cf8826beac2d591
Christiaan Seerden <christiaanseerden@live.nl>
2025-02-19 18:37:58 +0100

    extract PriceActionTable to its own file

---
+++ client/src/components/price-action/PriceActionTable/PriceActionTable.style.ts
@@ -0,0 +1,53 @@
+import { pad } from "@/lib/style/spacing";
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
+const TableWrapper = styled.div<{ columns: number }>`
+	margin: 1.5rem;
+	display: grid;
+	grid-template-columns: repeat(${(p) => p.columns}, max-content);
+
+	// temporary
+	outline: 2px solid orange;
+	padding: 1rem;
+`;
+
+export const subgridColumn = css`
+	display: grid;
+	grid-template-columns: subgrid;
+	grid-column: 1 / -1;
+`;
+
+const TableRow = styled.div`
+	${subgridColumn};
+
+	padding: 0rem 1rem;
+	gap: 1rem;
+`;
+
+const Cell = styled.div``;
+
+const TableHeader = styled(TableRow)`
+	font-weight: bold;
+
+	position: sticky;
+	top: 0;
+	border-top: 0.5rem solid #f2f2f2; // needs to match the backgorund color so that the elements scrolling behind it don't show up.
+
+	background-color: #ddd;
+
+	padding-block: 0.5rem;
+	margin-bottom: 0.5rem;
+`;
+
+const InnerCell = styled.div`
+	${pad.wide(0.3, 2)};
+`;
+
+export default {
+	TableWrapper,
+	TableRow,
+	Cell,
+	InnerCell,
+	TableHeader
+};
---
+++ client/src/lib/style/spacing.ts
@@ -0,0 +1,46 @@
+import { css } from "@emotion/react";
+
+type SpacingRule = "padding" | "margin" | "gap";
+
+/** Restrict the naming of spacing values to this union. Means we'll have access
+ *to e.g. "gap.small", "padding.larger", etc. */
+type SpacingKey = "smaller" | "small" | "large" | "larger";
+
+/** The allowed spacing values (in rem). */
+type SpacingSize = 0.2 | 0.3 | 0.5 | 1 | 2 | 3;
+
+/** The allowed ratios between width and height of wide/tall
+ * spacings. */
+type SpacingRatio = 1 | 2 | 3 | 5 | 8;
+
+const spacing: Record<SpacingKey, `${SpacingSize}rem`> = {
+	smaller: "0.3rem",
+	small: "0.5rem",
+	large: "1rem",
+	larger: "2rem"
+} as const;
+
+function wide(rule: SpacingRule) {
+	return (size: SpacingSize, ratio: SpacingRatio) => css`
+		${rule}-inline: ${+size * ratio}rem;
+		${rule}-block: ${size}rem;
+	`;
+}
+
+const spacingObject = (rule: SpacingRule) =>
+	({
+		smaller: {
+			[rule]: spacing.smaller
+		} as const,
+		small: {
+			[rule]: spacing.small
+		} as const,
+		large: {
+			[rule]: spacing.large
+		} as const,
+		wide: wide(rule)
+	}) as const;
+
+export const gap = spacingObject("gap");
+export const mar = spacingObject("margin");
+export const pad = spacingObject("padding");
