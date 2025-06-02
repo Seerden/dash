596250a84298c4acaeb3d53458ff61e9951064f9
Christiaan Seerden <christiaanseerden@live.nl>
2025-06-02 14:47:31 +0200

    listMissingAvailableFlatFiles: improve docs, filter on allowed historical lookback

--- server/src/lib/polygon/flatfiles/list-files.ts
+++ server/src/lib/polygon/flatfiles/list-files.ts
@@ -18,25 +18,20 @@ export async function listFlatFiles(folder: `${FOLDERS}`) {
 }
 
 /**
+ * For a given folder, list all files that _are downloaded_, but have not been
+ * parsed yet (i.e. are not smembers in the store yet).
+ *
  * @note my initial flatfiles implementation doesn't have a mechanism to get,
  * parse, insert, cache the flatfiles, so there is no mechanism yet to ensure
  * that the data is up-to-date. This function is a first step in that direction.
+ *
  * @todo test this function */
 export async function listMissingAvailableFlatFiles(folder: `${FOLDERS}`) {
-	// await getFiles({ folder, year: "2025", month: "2" }); // TODO: 1. I only
-	// had to run thise once to download the files for 2025, 2. the month doesn't
-	// have to be supplied, it downloads everything for the whole year anyway (I
-	// should note that in the function description 🫠)
-
 	const files = (await listFlatFiles(folder)).map((file) =>
 		yearMonthDayToString(parseDailyAggsFilename(file)),
 	);
 	const storedFiles = new Set(await flatFilesDailyAggsStore.list());
 	const missingFiles = files.filter((file) => !storedFiles.has(file));
-
-	console.log(files.length, storedFiles.size, missingFiles.length);
-	return missingFiles;
-
 	const fiveYearsAgo = dayjs().subtract(5, "year").add(1, "day");
 	return missingFiles.filter((file) => dayjs(file, "YYYY-MM-DD").isAfter(fiveYearsAgo));
 }
