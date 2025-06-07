97b11b759fd295888b477bc496c2727c076b1038
Christiaan Seerden <christiaanseerden@live.nl>
2025-06-07 20:00:22 +0200

    more tests for market date helpers

--- server/src/lib/datetime/market-time.test.ts
+++ server/src/lib/datetime/market-time.test.ts
@@ -4,7 +4,14 @@
  */
 
 import day from "@shared/lib/datetime/day";
-import { generateDateRange, maxPolygonLookback } from "@shared/lib/datetime/market-time";
+import {
+	generateDateRange,
+	generateMarketDateRange,
+	getNextMarketDate,
+	getPreviousMarketDate,
+	isMarketDay,
+	maxPolygonLookback,
+} from "@shared/lib/datetime/market-time";
 import { formatToYearMonthDay } from "@shared/lib/datetime/timestamp";
 
 describe("marketTime", () => {
@@ -12,19 +19,19 @@ describe("marketTime", () => {
 		it("should generate nothing if from and to are not separate dates in the right order", () => {
 			const from = day();
 			const to = from.subtract(1, "day");
-			const dates = generateDateRange(
-				formatToYearMonthDay(from),
-				formatToYearMonthDay(to),
-			);
+			const dates = generateDateRange({
+				from: formatToYearMonthDay(from),
+				to: formatToYearMonthDay(to),
+			});
 			expect(dates).toEqual([]);
 		});
 		it("should generate a range of dates including both `from` and `to`", () => {
 			const from = day("2025-12-25");
 			const to = day("2025-12-31");
-			const dates = generateDateRange(
-				formatToYearMonthDay(from),
-				formatToYearMonthDay(to),
-			);
+			const dates = generateDateRange({
+				from: formatToYearMonthDay(from),
+				to: formatToYearMonthDay(to),
+			});
 			expect(dates).toEqual([
 				"2025-12-25",
 				"2025-12-26",
@@ -33,18 +40,93 @@ describe("marketTime", () => {
 				"2025-12-29",
 				"2025-12-30",
 				"2025-12-31",
-			] as const);
+			]);
 		});
 	});
 
-	describe("generateMarketDateRange", () => {});
+	describe("generateMarketDateRange", () => {
+		it("generates a range of dates on a week containing a holiday", () => {
+			const from = day("2025-05-26");
+			const to = day("2025-06-02");
+			const dates = generateMarketDateRange({
+				from: formatToYearMonthDay(from),
+				to: formatToYearMonthDay(to),
+			});
+			expect(dates).toEqual([
+				"2025-05-27",
+				"2025-05-28",
+				"2025-05-29",
+				"2025-05-30",
+				"2025-06-02",
+			]);
+		});
 
-	describe("isMarketDay", () => {});
+		it("generates a range of dates on a week without a holiday", () => {
+			const from = day("2025-06-02");
+			const to = day("2025-06-08");
+			const dates = generateMarketDateRange({
+				from: formatToYearMonthDay(from),
+				to: formatToYearMonthDay(to),
+			});
+			expect(dates).toEqual([
+				"2025-06-02",
+				"2025-06-03",
+				"2025-06-04",
+				"2025-06-05",
+				"2025-06-06",
+			]);
+		});
+
+		it("returns an empty array if from and to are not separate dates in the right order", () => {
+			const from = day("2025-06-08");
+			const to = from.subtract(1, "day");
+			const dates = generateMarketDateRange({
+				from: formatToYearMonthDay(from),
+				to: formatToYearMonthDay(to),
+			});
+			expect(dates).toEqual([]);
+		});
+	});
+
+	describe("isMarketDay", () => {
+		it("should return true for a market day", () => {
+			expect(isMarketDay(formatToYearMonthDay(day("2025-01-02")))).toBe(true);
+		});
+
+		it("should return false for a weekend", () => {
+			expect(isMarketDay(formatToYearMonthDay(day("2025-01-04")))).toBe(false); // a Saturday
+		});
+
+		it("should return false for a holiday", () => {
+			expect(isMarketDay(formatToYearMonthDay(day("2025-01-01")))).toBe(false); // New Year's
+		});
+	});
 
 	describe("lastCompleteMarketSession", () => {});
 
-	describe("getPreviousMarketDate", () => {});
-	describe("getNextMarketDate", () => {});
+	describe("getPreviousMarketDate", () => {
+		it("should not see a holiday as a previous trading date", () => {
+			expect(getPreviousMarketDate("2025-01-02")).toEqual("2024-12-31"); // 2024/12/31 was a tuesday
+		});
+		it("should see a regular monday as a previous trading date w.r.t. a tuesday", () => {
+			expect(getPreviousMarketDate("2025-01-07")).toEqual("2025-01-06"); // 2025/01/06 was a monday
+		});
+		it("should see a friday as a previous trading date w.r.t. a monday", () => {
+			expect(getPreviousMarketDate("2025-01-06")).toEqual("2025-01-03"); // 2025/01/03 was a friday
+		});
+	});
+	// basically the inverse of getPreviousMarketDate
+	describe("getNextMarketDate", () => {
+		it("should not see a holiday as a next trading date", () => {
+			expect(getNextMarketDate("2025-01-01")).toEqual("2025-01-02"); // 2025/01/02 was a thursday
+		});
+		it("should see a regular monday as a next trading date w.r.t. a friday", () => {
+			expect(getNextMarketDate("2025-01-03")).toEqual("2025-01-06"); // 2025/01/06 was a monday
+		});
+		it("should see a tuesday as a next trading date w.r.t. a monday", () => {
+			expect(getNextMarketDate("2025-01-06")).toEqual("2025-01-07"); // 2025/01/07 was a tuesday
+		});
+	});
 
 	describe("toUnixMs", () => {});
 	describe("dayBefore", () => {});
