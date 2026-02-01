d5789ae9cb439f44a1eea2d6d8a37829789faedc
Christiaan Seerden <48338210+Seerden@users.noreply.github.com>
2026-02-01 14:43:15 +0100

    (DAS-58) trades-related database tables (#77)
    * (chore) list script cache filenames in sync script; (executeNewScripts) use createTransaction
    
    * implement trades-related database tables

---
+++ server/src/db/scripts/scripts/04.create-trades-tables.sql
@@ -0,0 +1,53 @@
+begin;
+
+create table if not exists trades (
+   id bigint generated always as identity primary key,
+   account text not null,
+   ticker text not null,
+   realized numeric(16,6) not null,
+   unrealized numeric(16,6),
+   duration numeric, -- is null when trade is open
+   closed boolean default false,
+   created_at timestamptz default now(),
+   updated_at timestamptz default now()
+);
+
+create table if not exists tickets (
+   id bigint generated always as identity primary key,
+   account text not null,
+   trade_id bigint references trades(id) on delete cascade,
+   "timestamp" timestamptz not null,
+   ticker varchar(10) not null,
+   amount numeric not null,
+   side varchar(4) not null, -- BC, SS, B, S; use an enum server-side probably?
+   price numeric(16,6) not null
+);
+
+create table if not exists trades_meta (
+   id bigint generated always as identity primary key,
+   account text not null,
+   ticker varchar(10) not null,
+   ticket_id bigint references tickets(id) on delete cascade,
+   "timestamp" timestamptz not null,
+   "data" jsonb, -- TODO (DAS-58): refine this, do we allow putting anything in here? 
+   "stop" numeric,
+   target numeric,
+   amount numeric,
+   created_at timestamptz default now(),
+   updated_at timestamptz default now()
+);
+
+create index on trades(account);
+create index on trades(ticker);
+create index on trades(closed);
+
+create index on tickets(account);
+create index on tickets(trade_id);
+create index on tickets(ticker);
+create index on tickets("timestamp");
+
+create index on trades_meta(account);
+create index on trades_meta(ticker);
+create index on trades_meta("timestamp");
+
+commit;
