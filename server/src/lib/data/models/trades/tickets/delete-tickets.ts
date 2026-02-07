00263619e10a6df097ab2cf2092356eecf67e3eb
Christiaan Seerden <christiaanseerden@live.nl>
2026-02-07 19:44:09 +0100

    (fix) actually roll back deleteTickets on unexpected output

--- server/src/lib/data/models/trades/tickets/delete-tickets.ts
+++ server/src/lib/data/models/trades/tickets/delete-tickets.ts
@@ -1,6 +1,6 @@
 import { TRADES_TABLES } from "@shared/types/table.types";
 import type { Ticket } from "@shared/types/trades.types";
-import { query } from "@/lib/query-function";
+import { createTransaction, query } from "@/lib/query-function";
 
 /** Delete one or more tickets from the database. */
 export const deleteTickets = query(async (sql, { ids }: { ids: string[] }) => {
@@ -9,17 +9,20 @@ export const deleteTickets = query(async (sql, { ids }: { ids: string[] }) => {
 	}
 
 	const uniqueIds = Array.from(new Set(ids));
-	const deletedTickets = await sql<Ticket[]>`
-      delete from ${sql(TRADES_TABLES.tickets)}
-      where id = any(${uniqueIds})
-      returning *
-   `;
 
-	if (deletedTickets.length !== uniqueIds.length) {
-		throw new Error(
-			`deleteTrades: rolling back; could not delete all trades given ids ${ids}`
-		);
-	}
+	return await createTransaction(async (sql) => {
+		const deletedTickets = await sql<Ticket[]>`
+         delete from ${sql(TRADES_TABLES.tickets)}
+         where id = any(${uniqueIds})
+         returning *
+      `;
 
-	return deletedTickets;
+		if (deletedTickets.length !== uniqueIds.length) {
+			throw new Error(
+				`deleteTrades: rolling back; could not delete all trades given ids ${ids}`
+			);
+		}
+
+		return deletedTickets;
+	});
 });
