345ba0076b18eaa6fda6ce1d81f514fc62af87e7
Christiaan Seerden <christiaanseerden@live.nl>
2026-02-07 19:52:08 +0100

    docstrings for trade meta query functions, and return data from queries

--- server/src/lib/data/models/trades/trades-meta/delete-trades-meta.ts
+++ server/src/lib/data/models/trades/trades-meta/delete-trades-meta.ts
@@ -2,11 +2,13 @@ import { TRADES_TABLES } from "@shared/types/table.types";
 import type { TradeMeta } from "@shared/types/trades.types";
 import { query } from "@/lib/query-function";
 
+/** Delete the given trades meta rows by id. */
 export const deleteTradesMeta = query(
 	async (sql, { ids }: { ids: string[] }) => {
 		return await sql<TradeMeta[]>`
          delete from ${TRADES_TABLES.meta}
          where id = any(${sql(ids)})
+         returning *
       `;
 	}
 );
